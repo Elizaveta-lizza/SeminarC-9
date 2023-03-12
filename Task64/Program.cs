d07b697b2fd8792bc1838ea1ed78a0ced15f3d5b
Liza <kuznecovaliza11@yandex.ru>
2023-03-12 18:16:24 +0700

    Задача 64

---
+++ Task64/Program.cs
@@ -0,0 +1,23 @@
+﻿int GetIncomingData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintPrevNaturalNumbers(int naturalnumber)
+{
+    if(naturalnumber > 1)
+    {
+        Console.Write($"{naturalnumber},");    
+        naturalnumber--;
+    }
+    else
+    {
+        Console.Write($"{naturalnumber}");    
+        return;
+    };
+    PrintPrevNaturalNumbers(naturalnumber);
+}
+
+int NaturalNumbers = GetIncomingData("Введите натуральное число");
+PrintPrevNaturalNumbers( NaturalNumbers );

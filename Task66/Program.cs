1915b375e7c88b5026e6e8ecc0d02b0bf8695de0
Liza <kuznecovaliza11@yandex.ru>
2023-03-12 18:16:32 +0700

    Задача 66

---
+++ Task66/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetIncomingData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumm(int naturalnumber, int high)
+{
+    naturalnumber++;
+    if (naturalnumber == high)
+    {
+        return naturalnumber;
+    };
+    return GetSumm(naturalnumber, high) + naturalnumber;
+}
+
+int Low = GetIncomingData("Введите нижнюю границу натуральным числом");
+int High = GetIncomingData("Введите верхнюю границу натуральным числом");
+Console.WriteLine(GetSumm(Low-1, High));

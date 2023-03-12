ce10bdce77036a2c12457dff28df7e6aca62cc9a
Liza <kuznecovaliza11@yandex.ru>
2023-03-12 18:16:48 +0700

    Задача 68

---
+++ Task68/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int GetIncomingData(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetAckerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if ((m != 0) && (n == 0))
+        {
+            return GetAckerman(m - 1, 1);
+        }
+        else
+        {
+            return GetAckerman(m - 1, GetAckerman(m, n - 1));
+        }
+    }
+}
+
+int m = GetIncomingData("Введите m");
+int n = GetIncomingData("Введите n");
+Console.WriteLine(GetAckerman(m, n));
